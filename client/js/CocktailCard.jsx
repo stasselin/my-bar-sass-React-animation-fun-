26ae44cfea24ef3a929935fd0f028613856869d2
Stanislav Selin <stasselin@yandex.ru>
2018-01-22 22:17:05 +0300

    first commit

--- client/js/CocktailCard.jsx
+++ client/js/CocktailCard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const CocktailCard = props => (
-  <div className="search-containter__cocktail-card">
-    <img src={props.cocktail.image} alt={`${props.cocktail.name}`} width="100%" />
-    <div>
-      <h3>{props.cocktail.name}</h3>
+  <div className="cocktail-card">
+    <div className="cocktail-card__side cocktail-card__side--front">
+      <img src={props.cocktail.image} alt={`${props.cocktail.name}`} />
+      <h3 className="cocktail-title">{props.cocktail.name}</h3>
+    </div>
+    <div className="cocktail-card__side cocktail-card__side--back">
       <p>{props.cocktail.recipe}</p>
     </div>
   </div>

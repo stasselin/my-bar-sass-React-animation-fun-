4a4c6eb8410cccd87557f8ab10a963854098f1e8
Stanislav Selin <stasselin@yandex.ru>
2018-02-03 16:53:18 +0300

    search made

--- client/js/Search.jsx
+++ client/js/Search.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 import CocktailCard from './CocktailCard';
 
-const Search = () => (
-  <div className="search-container">
-    {preload.map(cocktail => <CocktailCard key={cocktail._id} cocktail={cocktail} />)}
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ''
+    };
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+  render() {
+    return (
+      <div>
+        <header className="nav">
+          <h1 className="nav__brand">MyBar</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            className="nav__search"
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div className="search-container">
+          {preload
+            .filter(
+              cocktail =>
+                `${cocktail.name} ${cocktail.ingredient1} ${cocktail.ingredient2} ${cocktail.ingredient3} ${
+                  cocktail.ingredient4
+                } ${cocktail.ingredient5} ${cocktail.ingredient6} ${cocktail.ingredient7} ${cocktail.ingredient8} ${
+                  cocktail.ingredient9
+                } ${cocktail.ingredient10}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(cocktail => <CocktailCard key={cocktail._id} cocktail={cocktail} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
